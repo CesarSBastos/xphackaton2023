8d90005bfab09e458e4ec741daf8fc0ee2f484bd
CesarSBastos <cesarbastos32@gmail.com>
2023-07-02 10:10:13 -0300

    ajustar

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 const fs = require('fs');
 let somaGastos = 0
 let somaEntradas = 0
+let totalGastosFixos = 0;
+let totalGastosDesejosPessoais = 0;
+let totalProjetosFuturos = 0;
+let gastosIndispensaveis = ["Pagamento de conta de luz", "Pagamento de conta de água", "Aluguel", "Alimentação", "Pagamento de conta de gás"] // 50% 
+let gastosDesejos = ["Compra online", "Serviços de streaming"] //30%
+let reservaEmergencia = [] //20%
 
-    
+
 try {
     //Leitura do JSON
     const jsonString = fs.readFileSync('./mock.json', 'utf-8');
     const jsonData = JSON.parse(jsonString);
     //console.log(jsonData);
     let movimentacoes = jsonData.movimentacoes
+    let cliente = jsonData.cliente
+    let salarioCliente = cliente.salario
+
+    let metaFixo = (salarioCliente * 0.5)
+    let metaDesejos = (salarioCliente * 0.3)
+    let metaReserva = (salarioCliente * 0.2)
 
     //Soma dos valores gastos durante o mês 
     const gastos = movimentacoes.filter(obj => obj.tipo === "saida")
     const valorGasto = gastos.forEach(element => {
         somaGastos += element.valor
     });
-     
+
     //Soma das entradas de valores durante o mês
     const entradas = movimentacoes.filter(obj => obj.tipo === "entrada")
     const valorDeEntradas = entradas.forEach(element => {
@@ -28,42 +40,20 @@ try {
 
     // Percorrer os gastos e categorizá-los
     for (const gasto of gastos) {
+
         const categoria = gasto.descricao;
+        const valor = gasto.valor;
 
-        // Verificar se a categoria já existe no objeto gastosPorCategoria
-        if (gastosPorCategoria.hasOwnProperty(categoria)) {
-            // Se a categoria já existe, adiciona o gasto ao array correspondente
-            gastosPorCategoria[categoria].push(gasto);
-        } else {
-            // Se a categoria não existe, cria um novo array com o gasto
-            gastosPorCategoria[categoria] = [gasto];
+        if (gastosIndispensaveis.includes(categoria)) {
+            // Se o gasto pertence às categorias indispensáveis, adiciona ao total de gastos fixos
+            totalGastosFixos += valor;
+        } else if (gastosDesejos.includes(categoria)) {
+            // Caso contrário, adiciona ao total de gastos com desejos pessoais
+            totalGastosDesejosPessoais += valor;
         }
+
     }
-
-    // (Provisório) Exibir os gastos por categoria
-    for (const categoria in gastosPorCategoria) {
-        console.log(`Categoria: ${categoria}`);
-        console.log(gastosPorCategoria[categoria]);
-        console.log("-------------------------");
-    }
-
-
-    console.log("Valor gasto:" + somaGastos)
-    console.log("Valor de entradas: " + somaEntradas)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    callOpenAi(totalGastosFixos,totalGastosDesejosPessoais,totalProjetosFuturos,salarioCliente)
 
 } catch (error) {
     console.log('Ocorreu um erro ao ler o arquivo:', error);
@@ -71,42 +61,33 @@ try {
 
 
 
+function callOpenAi(gastosFixos,gastosDesejos,reservaEmergencial,salario) {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer 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");
+    myHeaders.append("Content-Type", "application/json");
 
+    var raw = JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": `Seja meu consultor financeiro, desejo organizar meus gastos com o método 50/30/20. Meus gastos fixos são: S${gastosFixos}, meus gastos com desejos pessoais são: ${gastosDesejos}, e minha reserva de emergencia ${reservaEmergencial}. Meu salario é de ${salario}, onde posso aprimorar para conseguir uma estabilidade financeira melhor. Me de uma resposta resumida com breves insights`
+            }
+        ]
+    });
 
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
 
 
 
